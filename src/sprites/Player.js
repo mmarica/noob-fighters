c4710148a6e8d9dd303dcc645e267b16cd08ee2d
Mihai Marica <mihai.marica82@yahoo.com>
2017-02-21 22:27:06 +0200

    added different keys for controlling the players

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -2,12 +2,47 @@ import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
 
-  constructor ({ game, x, y, key }) {
-    super(game, x, y, key)
-    this.anchor.setTo(0.5)
-  }
+    constructor ({ game, x, y, keys }) {
+        super(game, x, y, 'dude', 4)
+        this.anchor.setTo(0.5)
 
-  update () {
-  }
+        this.game.physics.arcade.enable(this);
+
+        this.body.bounce.y = 0.2;
+        this.body.gravity.y = 300;
+        this.body.collideWorldBounds = true;
+
+        this.animations.add('left', [0, 1, 2, 3], 10, true);
+        this.animations.add('right', [5, 6, 7, 8], 10, true);
+
+        this.keys = this.game.input.keyboard.addKeys(keys);
+    }
+
+    create () {
+    }
+
+    update () {
+        if (this.keys.left.isDown)
+        {
+            this.body.velocity.x = -150;
+            this.animations.play('left');
+        }
+        else if (this.keys.right.isDown)
+        {
+            this.body.velocity.x = 150;
+            this.animations.play('right');
+        }
+        else
+        {
+            this.animations.stop();
+            this.frame = 4;
+            this.body.velocity.x = 0;
+        }
+
+        if (this.keys.up.isDown) //&& this.players[0].body.touching.down
+        {
+            this.body.velocity.y = -350;
+        }
+    }
 
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -16,61 +16,27 @@ export default class extends Phaser.State {
     banner.smoothed = false
     banner.anchor.setTo(0.5)
 
-      this.players = [this.addPlayer(100), this.addPlayer(this.world.width - 100)]
+      this.players = [
+          this.game.add.existing(
+              new Player({
+                  game: this.game,
+                  x: 100,
+                  y: this.world.centerY,
+                  keys: { 'up': Phaser.KeyCode.W, 'down': Phaser.KeyCode.S, 'left': Phaser.KeyCode.A, 'right': Phaser.KeyCode.D }
+              })
+          ),
+          this.game.add.existing(
+              new Player({
+                  game: this.game,
+                  x: this.world.width - 100,
+                  y: this.world.centerY,
+                  keys: { 'up': Phaser.KeyCode.UP, 'down': Phaser.KeyCode.DOWN, 'left': Phaser.KeyCode.LEFT, 'right': Phaser.KeyCode.RIGHT }
+              })
+          )
+      ]
 
-      this.cursors = this.game.input.keyboard.createCursorKeys();
-  }
-
-  addPlayer(width) {
-      let player = this.game.add.existing(
-          new Player({
-              game: this.game,
-              x: width,
-              y: this.world.centerY,
-              key: 'dude'
-          })
-      )
-
-      this.game.physics.arcade.enable(player);
-
-      player.body.bounce.y = 0.2;
-      player.body.gravity.y = 300;
-      player.body.collideWorldBounds = true;
-
-       player.animations.add('left', [0, 1, 2, 3], 10, true);
-       player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-      return player;
   }
 
   update() {
-      this.players[0].body.velocity.x = 0;
-
-      if (this.cursors.left.isDown)
-      {
-          this.players[0].body.velocity.x = -150;
-          this.players[0].animations.play('left');
-      }
-      else if (this.cursors.right.isDown)
-      {
-          this.players[0].body.velocity.x = 150;
-          this.players[0].animations.play('right');
-      }
-      else
-      {
-          this.players[0].animations.stop();
-          this.players[0].frame = 4;
-      }
-
-      if (this.cursors.up.isDown) //&& this.players[0].body.touching.down
-      {
-          this.players[0].body.velocity.y = -350;
-      }
-  }
-
-  render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.players[0], 32, 32)
-    }
   }
 }
