3d13dd5af631479476450d5522b3eb47007be1a8
Mihai Marica <mihai.marica82@yahoo.com>
2017-03-13 20:58:40 +0200

    added key info to the start menu

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -10,6 +10,8 @@ export default class extends Phaser.State {
     create () {
         this._addBanner()
         this._addPressKeyToPlay()
+        this._addP1Keys()
+        this._addP2Keys()
 
         this.playKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         this.playKey.onDown.add(this.playKeyDown, this)
@@ -18,7 +20,7 @@ export default class extends Phaser.State {
     update() {}
 
     _addBanner () {
-        let text = new Phaser.Text(this.game, this.game.world.centerX, this.game.world.centerY, "Noob Fighters!")
+        let text = new Phaser.Text(this.game, this.game.world.centerX, this.game.world.centerY - 200, "Noob Fighters!")
         text.font = 'Russo One'
         text.fontSize = 80
         text.padding.set(10, 16)
@@ -29,13 +31,36 @@ export default class extends Phaser.State {
     }
 
     _addPressKeyToPlay () {
-        let text = new Phaser.Text(this.game, this.game.world.centerX, this.game.world.centerY + 80, "Press space to play")
+        let text = new Phaser.Text(this.game, this.game.world.centerX, this.game.world.centerY - 120, "Press space to play")
         text.font = 'Russo One'
         text.fontSize = 20
         text.fill = '#fff'
-        text.shadow = 2;
-        text.smoothed = true
         text.anchor.setTo(0.5, 0.5)
+        this.game.add.existing(text)
+    }
+
+    _addP1Keys () {
+        let x = 300
+        let y = this.game.world.centerY - 50
+        const SPACING = 35;
+
+        this._addPlayerText(x, y, "Player 1 keys\nLeft: Z\nRight: C\nJump: S\nPrimary: Left CTRL\nSecondary: Left SHIFT")
+    }
+
+    _addP2Keys () {
+        let x = this.game.world.centerX + 140
+        let y = this.game.world.centerY - 50
+        const SPACING = 35;
+
+        this._addPlayerText(x, y, "Player 2 keys\nLeft: Left ARROW\nRight: Right ARROW\nJump: Up ARROW\nPrimary: Right CTRL\nSecondary: Right SHIFT")
+    }
+
+    _addPlayerText (x, y, message) {
+        let text = new Phaser.Text(this.game, x, y, message)
+        text.font = 'Russo One'
+        text.fontSize = 20
+        text.padding.set(10, 5)
+        text.fill = '#fff'
         this.game.add.existing(text)
     }
 
