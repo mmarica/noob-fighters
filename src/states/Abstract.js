c3373fc29678b557d7290010633009d335039734
Mihai Marica <mihai.marica82@yahoo.com>
2017-03-29 03:33:50 +0300

    updated the way loading progress is displayed

--- src/states/Abstract.js
+++ src/states/Abstract.js
@@ -10,9 +10,9 @@ export default class extends Phaser.State {
         this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
         this.loaderBg.anchor.setTo(0.5)
 
-        this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
-        this.loaderBar.anchor.setTo(0.5)
-
+        let image = this.game.cache.getImage('loaderBar')
+        this.loaderBar = this.add.sprite(this.game.world.centerX - image.width / 2, this.game.world.centerY, 'loaderBar')
+        this.loaderBar.anchor.setTo(0, 0.5)
         this.load.setPreloadSprite(this.loaderBar)
     }
 }
