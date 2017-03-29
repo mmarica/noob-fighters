eb402de7e0d9efc0e08e7f083f855aa835b9bfab
Mihai Marica <mihai.marica82@yahoo.com>
2017-03-29 03:28:31 +0300

    made improvements to the boot state class

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -3,13 +3,23 @@ import WebFont from 'webfontloader'
 import AssetLoader from '../objects/AssetLoader'
 
 export default class extends Phaser.State {
-    init () {
+    /**
+     * Initialize
+     */
+    init() {
         this.stage.backgroundColor = '#000'
         this.fontsReady = false
         this.fontsLoaded = this.fontsLoaded.bind(this)
     }
 
-    preload () {
+    /**
+     * Load assets
+     */
+    preload() {
+        let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
+        text.anchor.setTo(0.5, 0.5)
+
+        // load fonts
         WebFont.load({
             google: {
                 families: ['Paytone One', 'Russo One']
@@ -17,14 +27,15 @@ export default class extends Phaser.State {
             active: this.fontsLoaded
         })
 
-        let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
-        text.anchor.setTo(0.5, 0.5)
-
+        // load images for the loading progress bar
         this.load.image('loaderBg', './assets/common/images/loader-bg.png?__version__')
         this.load.image('loaderBar', './assets/common/images/loader-bar.png?__version__')
     }
 
-    create () {
+    /**
+     * Create the scene
+     */
+    create() {
         // capture all letter, arrow and control keys, just to be sure
         this.game.input.keyboard.addKeyCapture([
             Phaser.KeyCode.Q, Phaser.KeyCode.W, Phaser.KeyCode.E, Phaser.KeyCode.R, Phaser.KeyCode.T, Phaser.KeyCode.Y, Phaser.KeyCode.U, Phaser.KeyCode.I, Phaser.KeyCode.O, Phaser.KeyCode.P,
@@ -37,14 +48,18 @@ export default class extends Phaser.State {
         AssetLoader.setGame(this.game)
     }
 
-    render () {
-        if (this.fontsReady) {
+    /**
+     * Wait until the fonts are loaded, then proceed to the next scene
+     */
+    render() {
+        if (this.fontsReady)
             this.state.start('Splash')
-        }
     }
 
-    fontsLoaded () {
+    /**
+     * Fonts loaded state
+     */
+    fontsLoaded() {
         this.fontsReady = true
     }
-
 }
